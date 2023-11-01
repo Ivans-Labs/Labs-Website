d28a353a8f9a6e5340f197031cbf17d1382acb7b
Ivan Ryan <ivan@ivanryan.dev>
2023-11-01 15:00:29 +0100

    update: components

--- components.tsx
+++ components.tsx
@@ -1,6 +1,22 @@
 /** @jsx femtojsx.h */
 
 import { femtojsx } from "./html.tsx";
+import { extract, install } from "https://esm.sh/@twind/core@1.1.3";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
+
+install({
+  presets: [
+    presetTailwind(),
+    {
+      theme: {
+        fontFamily: {
+          sans: ["Helvetica", "sans-serif"],
+          serif: ["Times", "serif"],
+        },
+      },
+    },
+  ],
+});
 
 type PageProps = {
   title?: string;
@@ -16,34 +32,64 @@ export function Page(props: PageProps) {
     <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {props.title && <title>{props.title}</title>}
         <link rel="stylesheet" href="https://the.missing.style" />
         <script src="https://unpkg.com/htmx.org@1.8.0" />
         {props.headExtras}
       </head>
-      <body>
-        {props.children}
+      <body style="background-color: #2A2E35; color: #FFF;">
+        <Header />
+        <main>
+          {props.children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
 }
 
+export function Header() {
+    return (
+        <header class="p-3" style="border-radius: 5px; background-color: #545B68;">
+            <h1>Ivan's Minimal Website</h1>
+        </header>
+    );
+}
+
+export function Footer() {
+    return (
+        <footer class="p-3 text-center" style="border-radius: 5px; background-color: #545B68;">
+            <p>&copy; 2023 Ivan's Minimal HTMX + Deno Website</p>
+        </footer>
+    );
+}
 
 export function HomePage() {
   return (
     <Page title="Ivan's Minimal Website">
-      <main class="text-center py-5" style="background-color: #2A2E35; color: #FFF;">
-        <h1>Welcome to Ivan's Minimal Website</h1>
-        <p class="mt-5">
-          <Counter value={0} />
-        </p>
-      </main>
+      <AboutSection />
+      <ContactSection />
     </Page>
   );
+}
+
+export function AboutSection() {
+    return (
+        <section class="p-5 text-center">
+            <h2>About Ivan's Minimal Website</h2>
+            <p>Welcome to my website! Here you can find information about me and my work.</p>
+        </section>
+    );
+}
+
+export function ContactSection() {
+    return (
+        <section class="p-5 text-center">
+            <h2>Contact Ivan</h2>
+            <p>For any inquiries or questions, please contact me through email@example.com. I'd love to hear from you!</p>
+        </section>
+    );
 }
 
 export function Counter(props: { value: number }) {
