3877587ec051cd9c8429136338426323748f2de0
Ivan Ryan <ivan@ivanryan.dev>
2023-11-04 22:56:32 +0100

    update: docs index

--- routes/docs/index.tsx
+++ routes/docs/index.tsx
@@ -9,22 +9,25 @@ const Docs = () => {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <title>My Fresh app</title>
+        <title>Ivans Labs - Docs</title>
       </head>
-
-      <body f-client-nav className={tw`flex`}>
+      
+      <body f-client-nav className={tw`flex bg-gray-800 text-white`}>
         <Partial name="sidebar">
           <aside className={tw`w-1/4 p-4`}>
-            <h2 className={tw`font-semibold text-lg mb-4`}>Index</h2>
-            <a href="/docs/routes" f-partial="/partials/docs/routes"> Routes </a>
+            <h1 className={tw`text-white text-2xl tracking-wide mb-4`}>Documentation Index</h1>
+            <h2 className={tw`text-white text-xl mb-2`}>Topics:</h2>
+            <a href="/docs/routes" f-partial="/partials/docs/routes" className={tw`text-blue-500 hover:text-blue-800`}>
+              Routes 
+            </a>
           </aside>
         </Partial>
 
         <Partial name="main">
-          <main className={tw`w-3/4 p-4`}>
-            <h2 className={tw`font-semibold text-lg mb-4`}>Document Title</h2>
-            <p> Documentation content goes here. </p>
+          <main className={tw`w-3/4 p-4 bg-white bg-opacity-90 rounded-lg m-4`}>
+            <h2 className={tw`font-semibold text-gray-800 mb-2`}>Document Title</h2>
+            <h3 className={tw`font-medium text-gray-700 mb-1`}>Subtitle</h3>
+            <p className={tw`text-gray-600`}> Documentation content goes here. </p>
           </main>
         </Partial>
       </body>
