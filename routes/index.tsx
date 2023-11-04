4c05a9d9d28539d907ab3f9c3713e6396230cadb
Ivan Ryan <ivan@ivanryan.dev>
2023-11-04 14:40:50 +0100

    update: Routes

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
-import { useSignal } from "@preact/signals";
 import { ProjectsView } from "../components/ProjectsView.tsx";
+import { News } from "../islands/News.tsx";
+import { NavBar } from "../components/NavBar.tsx";
 
 export default function Home() {
   return (
@@ -7,9 +8,10 @@ export default function Home() {
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <h1 class="text-4xl font-bold ">Welcome to Ivan's Labs</h1>
         <p class="my-4">
-          Interactively Updating Project Website based on Deno Fresh.
+          Minimal & Secure Website based on Deno Fresh tailored towards text-based browsers.
         </p>
       </div>
+      <News />
       <ProjectsView />
     </div>
   );
