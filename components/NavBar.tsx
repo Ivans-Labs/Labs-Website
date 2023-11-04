a366b5501bfadf237ccb851ffc1e4da8e861a9e3
Ivan Ryan <ivan@ivanryan.dev>
2023-11-04 22:53:49 +0100

    update: NavBar Component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,10 +3,16 @@ import { tw } from 'twind';
 
 export function NavBar() {
     return (
-        <nav className={tw`bg-gray-900 rounded-full text-white p-2 flex justify-center`}>
-            <a className={tw`mx-1`} href="#">Home</a>
-            <a className={tw`mx-1`} href="#">About</a>
-            <a className={tw`mx-1`} href="#">Contact</a>
-        </nav>
+        <ul class={tw`flex justify-center fixed top-4 z-50 rounded-full bg-black bg-opacity-90 px-3 text-sm font-medium text-white shadow-lg ring-1 ring-black backdrop-blur`}>
+            <li>
+                <a class={tw`relative block px-3 py-2 transition hover:text-teal-500`} href="/">
+                    Home
+                    <span class={tw`absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0`}></span>
+                </a>
+            </li>
+            <li><a class={tw`relative block px-3 py-2 transition hover:text-teal-500`} href="/reports">Reports</a></li>
+            <li><a class={tw`relative block px-3 py-2 transition hover:text-teal-500`} href="/news">News</a></li>
+            <li><a class={tw`relative block px-3 py-2 transition hover:text-teal-500`} href="/docs">Docs</a></li>
+        </ul>
     );
 }
