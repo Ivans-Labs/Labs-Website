87e8999ef24d10a837f35fcd4f2678097f08afff
Ivan Ryan <ivan@ivanryan.dev>
2023-11-04 14:40:41 +0100

    update: ProjectsView

--- components/ProjectsView.tsx
+++ components/ProjectsView.tsx
@@ -1,6 +1,5 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
-
 interface Project {
   title: string;
   image: string;
@@ -13,22 +12,35 @@ export function ProjectsView() {
   const projects: Project[] = [
     {
       title: "simple-pdf-app",
-      image: "https://camo.githubusercontent.com/80ffec709b1a2f1908fe3f2f62de2defbe04ab4eb17a1a1263db8f228684a07f/68747470733a2f2f63646e2e737461746963616c6c792e696f2f67682f5375646f2d4976616e2f4d79576562736974652d4173736574732f6d61696e2f696d616765732f776562736974652f706466617070686f6d65706167652e706e67",
+      image: "https://cdn.statically.io/gh/Sudo-Ivan/MyWebsite-Assets/main/images/website/pdfmergershowcase1.png",
       githubLink: "https://github.com/Ivans-Labs/simple-pdf-app",
       version: "1.0",
       lastUpdated: "2023-03-03"
-    }
-    // more projects...
+    },
+    {
+      title: "Titan OSINT Arsenal",
+      image: "https://cdn.statically.io/gh/Sudo-Ivan/MyWebsite-Assets/main/images/logo/titanosint.png",
+      githubLink: "https://github.com/Ivans-Labs/",
+      version: "0.1",
+      lastUpdated: "2023-06-01"
+    },
+    {
+      title: "DataHindge",
+      image: "https://cdn.statically.io/gh/Sudo-Ivan/MyWebsite-Assets/main/images/logo/datahinge.png",
+      githubLink: "https://github.com/Ivans-Labs/",
+      version: "0.6",
+      lastUpdated: "2023-09-01"
+    },
   ];
 
   return (
-    <div class="flex flex-wrap justify-around text-white">
+    <div class="flex flex-wrap justify-around text-white mt-12">
       {projects.map((project) => (
         <div
           key={project.title}
-          class="m-4 rounded-lg border-2 border-gray-300 p-4 max-w-sm shadow-lg"
+          class="m-2 rounded-lg border-2 border-gray-700 p-4 max-w-sm shadow-lg bg-[#032b52]"
         >
-          <img src={project.image} alt={project.title} class="mb-4 rounded-lg" />
+          <img src={project.image} alt={project.title} class="mb-4 rounded-lg" style={{width: '400px', height: 'max'}} />
           <h3 class="text-lg mb-2">{project.title}</h3>
           <p><strong>Version:</strong> {project.version}</p>
           <p><strong>Last Updated:</strong> {project.lastUpdated}</p>
@@ -49,7 +61,7 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-black hover:bg-gray-200 transition-colors"
+      class="px-2 py-1 border-gray-700 border-2 rounded bg-black hover:bg-gray-700 transition-colors text-white"
     />
   );
 }
