6eb4607d0e910ae3bb4fabdaa43a837bc40a57f9
Ivan Ryan <ivan@ivanryan.dev>
2023-11-05 01:43:44 +0100

    Style: Center Navbar

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,9 +6,11 @@ import { News } from "../islands/News.tsx";
 export default function Home() {
   return (
     <div class={tw`px-4 py-8 mx-auto bg-[#03396c] text-white min-h-screen`}>
-      <NavBar />
+      <div class={tw`w-full flex justify-center`}>
+        <NavBar />
+      </div>
       <div class={tw`pt-14 max-w-screen-lg mx-auto flex flex-col items-center justify-center`}>
-        <h1 class={tw`text-6xl font-bold leading-snug mb-4 text-white`}>Welcome to Ivan's Labs</h1>
+        <h1 class={tw`text-5xl font-bold leading-snug mb-4 text-white`}>Welcome to Ivan's Labs</h1>
         <p class={tw`my-4 text-lg leading-relaxed text-gray-200`}>
           Secure & Refreshing Website powered by Deno Fresh Framework.
         </p>
